f2d01afa78aaafb9acb6e383e5573aa3866764a7
runehero <runehero@hotmail.com>
2024-06-06 18:56:59 +0200

    Created a DbContextFactory to support scaffolding of views in man project.

---
+++ FribergbookRentals.Data/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FribergBookRentals.Data
+{
+	public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+	{
+		public ApplicationDbContext CreateDbContext(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+			.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+			.AddJsonFile("appsettings.Development.json")
+			.Build();
+
+			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+			optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+			return new ApplicationDbContext(optionsBuilder.Options);
+		}
+	}
+}
