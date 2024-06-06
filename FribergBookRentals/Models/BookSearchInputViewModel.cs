1bfdecdfc63663a6d162e343c70060bf1a28ef40
runehero <runehero@hotmail.com>
2024-06-06 21:54:18 +0200

    Created view models for searching for and presenting books.

---
+++ FribergBookRentals/Models/BookSearchInputViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FribergbookRentals.Models
+{
+	public class BookSearchInputViewModel
+	{
+		#region Constructors
+
+		public BookSearchInputViewModel(string? searchPhrase = null, string? language = null, int? year = null)
+		{
+			SearchPhrase = searchPhrase;
+			Language = language;
+			Year = year;
+		}
+
+		#endregion
+
+		#region Properties
+
+		public string? SearchPhrase { get; set; } = "";
+
+		public string? Language { get; set; } = "";
+
+		public int? Year { get; set; }
+
+		#endregion
+	}
+}
---
+++ FribergBookRentals/Models/BookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FribergbookRentals.Models
+{
+	public class BookViewModel
+	{
+        #region Properties
+
+        public string Author { get; set; } = "";
+
+        public int BookId { get; set; }
+
+		public string Country { get; set; } = "";
+
+        public string? InformationUrl { get; set; } = null;
+
+        public string Language { get; set; } = "";
+
+        public int NumberOfPages { get; set; }
+
+		public string Title { get; set; } = "";
+
+		public int Year { get; set; }
+
+        #endregion
+	}
+}
