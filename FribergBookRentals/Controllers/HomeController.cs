94bf43d590d55a67c666540fdf2fb7e431de1392
runehero <runehero@hotmail.com>
2024-06-06 22:04:54 +0200

    Implemented actions to support searching for books in the HomeController.

--- FribergBookRentals/Controllers/HomeController.cs
+++ FribergBookRentals/Controllers/HomeController.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using FribergbookRentals.Data.Models;
+using FribergbookRentals.Data.Repositories;
+using FribergbookRentals.Models;
 using FribergBookRentals.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,20 +10,51 @@ namespace FribergBookRentals.Controllers
 {
 	public class HomeController : Controller
 	{
-		public HomeController()
-		{
-            
+        #region Fields
+
+        IMapper _autoMapper;
+
+        IBookRepository _bookRepository;
+
+        #endregion
+
+        #region Constructors
+
+        public HomeController(IBookRepository bookRepository, IMapper autoMapper)
+        {
+            _bookRepository = bookRepository;
+            _autoMapper = autoMapper;
         }
 
-		public IActionResult Index()
-		{			
-			return View();
-		}
+        #endregion
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        #region Actions
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(new List<BookViewModel>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(BookSearchInputViewModel searchInput)
+        {
+            List<BookViewModel> books = new();
+
+            if (ModelState.IsValid)
+            {
+                var searchInputDto = _autoMapper.Map<BookSearchInputDto>(searchInput);
+                books = _autoMapper.Map<List<BookViewModel>>(await _bookRepository.SearchBooksAsync(searchInputDto));
+            }
+
+            return View(books);
+        }
+
+        #endregion
+    }
 }
