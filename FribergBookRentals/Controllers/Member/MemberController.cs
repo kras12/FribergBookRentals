3c8eab714ae55bb8b21e3b78e2c41b3363fe16f4
runehero <runehero@hotmail.com>
2024-06-06 18:58:26 +0200

    Scaffolded an index page (template) for the start page of the members back office.

---
+++ FribergBookRentals/Controllers/Member/MemberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FribergBookRentals.Controllers.Member
+{
+    public class MemberController : Controller
+    {
+        // GET: MemberController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
