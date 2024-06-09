c5909cd9b2e16b788474711bdb7708540d2aae62
runehero <runehero@hotmail.com>
2024-06-09 22:29:25 +0200

    TestMemberController: Added tests for closing and prolonging book loans.

--- FribergBookRentalsTest/Tests/Controllers/TestMemberController.cs
+++ FribergBookRentalsTest/Tests/Controllers/TestMemberController.cs
@@ -1,17 +1,11 @@
-﻿using AutoMapper;
-using FribergbookRentals.Data.Constants;
+﻿using FribergbookRentals.Data.Exceptions;
 using FribergbookRentals.Data.Models;
 using FribergbookRentals.Data.Repositories;
-using FribergBookRentals.Controllers;
 using FribergBookRentals.Controllers.Member;
-using FribergBookRentals.Mapper;
 using FribergBookRentals.Models;
 using FribergBookRentals.Services;
 using FribergBookRentalsTest.Helpers;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using System.Security.Claims;
 
@@ -45,6 +39,47 @@ namespace FribergBookRentalsTest.Tests.Controllers
 
         #region Methods
 
+        private async Task SeedBookLoans(User user, int activeLoans = 0, int closedLoans = 0, TimeSpan? startTimeOffset = null)
+        {
+            var books = await _bookRepository.GetBooksAsync();
+            DateTime startTime = startTimeOffset != null ? DateTime.Now.Add(startTimeOffset.Value) : DateTime.Now;
+
+            if (activeLoans > 0)
+            {
+                await BookLoanHelper.CreateBookLoans(_bookLoanRepository, user, books.Take(activeLoans).ToList(), createActiveLoans: true, startTime, TimeSpan.FromDays(BookLoanTime - 1));
+            }
+
+            if (closedLoans > 0)
+            {
+                await BookLoanHelper.CreateBookLoans(_bookLoanRepository, user, books.Skip(activeLoans).Take(closedLoans).ToList(), createActiveLoans: false, startTime, TimeSpan.FromDays(BookLoanTime - 1));
+            }            
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static IEnumerable<object[]> GetTestProlongActiveBookLoanInputData()
+        {
+            List<object[]> data = new List<object[]>();
+
+            for (int i = -BookLoanTime + 1; i <= 0; i++)
+            {
+                data.Add(new object[] { true, i });
+            }
+
+            for (int i = -BookLoanTime + 1; i <= 0; i++)
+            {
+                data.Add(new object[] { false, i });
+            }
+
+            return data;
+        }
+
+        #endregion
+
+        #region Tests
+
         private async Task<MemberController> CreateMemberController(bool isUserLoggedIn)
         {
             // User
@@ -68,14 +103,11 @@ namespace FribergBookRentalsTest.Tests.Controllers
         public async Task TestListBookLoans(bool isUserAuthenticated)
         {
             // Arrange
-            var user = await GetDefaultUser();
-            var memberController = await CreateMemberController(isUserAuthenticated);
-            var books = await _bookRepository.GetBooksAsync();
             int activeLoans = 10;
             int closedLoans = 8;
-
-            await BookLoanHelper.CreateBookLoans(_bookLoanRepository, user, books.Take(activeLoans).ToList(), createActiveLoans: true, DateTime.Now, DateTime.Now.AddDays(BookLoanTime - 1));
-            await BookLoanHelper.CreateBookLoans(_bookLoanRepository, user, books.Skip(activeLoans).Take(closedLoans).ToList(), createActiveLoans: false, DateTime.Now, DateTime.Now.AddDays(BookLoanTime - 1));
+            var user = await GetDefaultUser();
+            var memberController = await CreateMemberController(isUserAuthenticated);
+            await SeedBookLoans(user, activeLoans: activeLoans, closedLoans: closedLoans);
 
             // Act
             var result = await memberController.Index();
@@ -96,7 +128,170 @@ namespace FribergBookRentalsTest.Tests.Controllers
             else
             {
                 Assert.IsType<UnauthorizedResult>(result);
+            }
+        }
+
+        [InlineData(true)]
+        [InlineData(false)]
+        [Theory]
+        public async Task TestCloseActiveBookLoan(bool isUserAuthenticated)
+        {
+            // Arrange
+            int numLoans = 1;
+            var user = await GetDefaultUser();
+            var memberController = await CreateMemberController(isUserAuthenticated);
+            await SeedBookLoans(user, activeLoans: numLoans);
+            BookLoan targetBookLoan = (await _bookLoanRepository.GetActiveBookLoansAsync(user.Id)).First();
+
+            // Act
+            var result = await memberController.CloseLoan(targetBookLoan.Id);
+            var closedBookLoan = await _bookLoanRepository.GetBookLoanByIdAsync(targetBookLoan.Id);
+
+            // Assert
+            if (isUserAuthenticated)
+            {
+                Assert.IsType<RedirectToActionResult>(result);
+                Assert.NotNull(closedBookLoan!.ClosedTime);
+                Assert.Equal(targetBookLoan.StartTime, closedBookLoan.StartTime);
+                Assert.Equal(targetBookLoan.EndTime, closedBookLoan.EndTime);
+            }
+            else
+            {
+                Assert.IsType<UnauthorizedResult>(result);
             }            
+        }
+
+        [InlineData(true)]
+        [InlineData(false)]
+        [Theory]
+        public async Task TestCloseClosedBookLoan(bool isUserAuthenticated)
+        {
+            // Arrange
+            int numLoans = 1;
+            var user = await GetDefaultUser();
+            var memberController = await CreateMemberController(isUserAuthenticated);
+            await SeedBookLoans(user, closedLoans: numLoans);
+            BookLoan targetBookLoan = (await _bookLoanRepository.GetClosedBookLoansAsync(user.Id)).First();
+
+            // Act
+            var result = await memberController.CloseLoan(targetBookLoan.Id);
+            var closedBookLoan = await _bookLoanRepository.GetBookLoanByIdAsync(targetBookLoan.Id);
+
+            // Assert
+            if (isUserAuthenticated)
+            {
+                Assert.IsType<NotFoundResult>(result);
+                Assert.NotNull(closedBookLoan!.ClosedTime);
+                Assert.Equal(targetBookLoan.StartTime, closedBookLoan.StartTime);
+                Assert.Equal(targetBookLoan.EndTime, closedBookLoan.EndTime);
+            }
+            else
+            {
+                Assert.IsType<UnauthorizedResult>(result);
+            }
+        }
+
+        [InlineData(true)]
+        [InlineData(false)]
+        [Theory]
+        public async Task TestCloseNonExistentBookLoan(bool isUserAuthenticated)
+        {
+            // Arrange
+            var memberController = await CreateMemberController(isUserAuthenticated);
+
+            // Act
+            var result = await memberController.CloseLoan(0);
+
+            // Assert
+            if (isUserAuthenticated)
+            {
+                Assert.IsType<NotFoundResult>(result);
+            }
+            else
+            {
+                Assert.IsType<UnauthorizedResult>(result);
+            }
+        }
+
+        [MemberData(nameof(GetTestProlongActiveBookLoanInputData))]
+        [Theory]
+        public async Task TestProlongActiveBookLoan(bool isUserAuthenticated, int startTimeOffsetDays)
+        {
+            // Arrange
+            int numLoans = 1;
+            var user = await GetDefaultUser();
+            var memberController = await CreateMemberController(isUserAuthenticated);
+            DateTime seedTime = DateTime.Now;
+            await SeedBookLoans(user, activeLoans: numLoans, startTimeOffset: TimeSpan.FromDays(startTimeOffsetDays));
+            BookLoan targetBookLoan = (await _bookLoanRepository.GetActiveBookLoansAsync(user.Id)).First();
+
+            // Act
+            var result = await memberController.ProlongLoan(targetBookLoan.Id);
+            var prolongedBookLoan = await _bookLoanRepository.GetBookLoanByIdAsync(targetBookLoan.Id);
+
+            // Assert
+            if (isUserAuthenticated)
+            {
+                Assert.IsType<RedirectToActionResult>(result);
+                Assert.Null(targetBookLoan!.ClosedTime);
+                Assert.Equal(targetBookLoan.StartTime, prolongedBookLoan!.StartTime);
+                Assert.True(prolongedBookLoan!.EndTime > seedTime.AddDays(BookLoanTime - 1) && prolongedBookLoan.EndTime < seedTime.AddDays(BookLoanTime - 1).AddSeconds(1));
+            }
+            else
+            {
+                Assert.IsType<UnauthorizedResult>(result);
+            }
+        }
+
+        [InlineData(true)]
+        [InlineData(false)]
+        [Theory]
+        public async Task TestProlongClosedBookLoan(bool isUserAuthenticated)
+        {
+            // Arrange
+            int numLoans = 1;
+            var user = await GetDefaultUser();
+            var memberController = await CreateMemberController(isUserAuthenticated);
+            DateTime seedTime = DateTime.Now;
+            await SeedBookLoans(user, closedLoans: numLoans);
+            BookLoan targetBookLoan = (await _bookLoanRepository.GetClosedBookLoansAsync(user.Id)).First();
+
+            if (isUserAuthenticated)
+            {
+                // Act
+                // Assert
+                await Assert.ThrowsAsync<BookLoanClosedException>(async () => await memberController.ProlongLoan(targetBookLoan.Id));
+            }
+            else
+            {
+                // Act
+                var result = await memberController.ProlongLoan(targetBookLoan.Id);
+
+                // Assert
+                Assert.IsType<UnauthorizedResult>(result);
+            }
+        }
+
+        [InlineData(true)]
+        [InlineData(false)]
+        [Theory]
+        public async Task TestProlongNonExistentBookLoan(bool isUserAuthenticated)
+        {
+            // Arrange
+            var memberController = await CreateMemberController(isUserAuthenticated);
+
+            // Act
+            var result = await memberController.ProlongLoan(0);
+
+            // Assert
+            if (isUserAuthenticated)
+            {
+                Assert.IsType<NotFoundResult>(result);
+            }
+            else
+            {
+                Assert.IsType<UnauthorizedResult>(result);
+            }
         }
 
         #endregion
